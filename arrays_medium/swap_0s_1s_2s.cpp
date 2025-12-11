969a08b2a57d9ba51476c7ddadfaf87a9c0e1c02
Khushi Khatale <khushikhatale0310@gmail.com>
2025-12-11 21:23:29 +0530

    Add files via upload

---
+++ arrays_medium/swap_0s_1s_2s.cpp
@@ -0,0 +1,36 @@
+/*Sort an array of 0's 1's and 2's
+Given an array nums consisting of only 0, 1, or 2. Sort the array in non-decreasing order.
+The sorting must be done in-place, without making a copy of the original array.
+Examples:
+Input: nums = [1, 0, 2, 1, 0]
+Output: [0, 0, 1, 1, 2]
+Explanation:
+The nums array in sorted order has 2 zeroes, 2 ones and 1 two
+Input: nums = [0, 0, 1, 1, 1]
+Output: [0, 0, 1, 1, 1]
+Explanation:
+The nums array in sorted order has 2 zeroes, 3 ones and zero twos*/
+
+class Solution {
+public:
+    void sortZeroOneTwo(vector<int>& nums) {
+        int low = 0;
+        int mid = 0;
+        int high = nums.size() - 1;
+
+        while(mid <= high){
+            if(nums[mid] == 0){
+                swap(nums[low], nums[mid]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid] == 1){
+                mid++;
+            }
+            else{
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+        }
+    }
+};
