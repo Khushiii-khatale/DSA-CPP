94b9eddec39475ef1b74a5bce04596dfbaf5dc4d
Khushi Khatale <khushikhatale0310@gmail.com>
2025-12-14 11:38:29 +0530

    Add files via upload

---
+++ arrays_medium/leader_in_array.cpp
@@ -0,0 +1,26 @@
+/*Leaders in an Array
+Given an integer array nums, return a list of all the leaders in the array.
+A leader in an array is an element whose value is strictly greater than all elements to its right in the given array. The rightmost element is always a leader. The elements in the leader array must appear in the order they appear in the nums array.
+Example 1
+Input: nums = [1, 2, 5, 3, 1, 2]
+Output: [5, 3, 2]
+Explanation:
+2 is the rightmost element, 3 is the largest element in the index range [3, 5], 5 is the largest element in the index range [2, 5]*/
+
+class Solution {
+public:
+    vector<int> leaders(vector<int>& nums) {
+      int maxi = INT_MIN;
+      int n = nums.size();
+      vector <int> ans;
+
+      for(int i = n-1; i >= 0; i--){
+        if(nums[i] > maxi){
+            ans.push_back(nums[i]);
+        }
+        maxi = max(maxi, nums[i]);
+      }
+      reverse(ans.begin(), ans.end());
+      return ans;
+    }
+};
---
+++ arrays_medium/rearrange_arr_by_sign.cpp
@@ -0,0 +1,31 @@
+/*Rearrange array elements by sign
+Given an integer array nums of even length consisting of an equal number of positive and negative integers.Return the answer array in such a way that the given conditions are met:
+Every consecutive pair of integers have opposite signs.
+For all integers with the same sign, the order in which they were present in nums is preserved.
+The rearranged array begins with a positive integer.
+Example 1
+Input : nums = [2, 4, 5, -1, -3, -4]
+Output : [2, -1, 4, -3, 5, -4]
+Explanation:
+The positive number 2, 4, 5 maintain their relative positions and -1, -3, -4 maintain their relative positions*/
+
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int n = nums.size();
+        vector <int> ans(n, 0);
+        int posIndex = 0, negIndex = 1;
+
+        for(int i=0; i<n; i++){
+            if(nums[i] < 0){
+                ans[negIndex] = nums[i];
+                negIndex += 2;
+            }
+            else{
+                ans[posIndex] = nums[i];
+                posIndex += 2;
+            }
+        }
+        return ans;
+    }
+};
